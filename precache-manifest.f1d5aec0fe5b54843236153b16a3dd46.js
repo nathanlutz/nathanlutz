7f430a6d26558fc8fa95e31efcc8a4b59734e4c3
Nathan Lutz <nathanl@berkeley.edu>
2020-10-04 15:20:51 -0700

    Updates

---
+++ precache-manifest.f1d5aec0fe5b54843236153b16a3dd46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca0d110d1dcffa019cef2ef74a800bed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a03c12f72b0f88e3a9c5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f747cad808322017b88e",
+    "url": "/static/js/2.13545573.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.13545573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a03c12f72b0f88e3a9c5",
+    "url": "/static/js/main.f5e85fae.chunk.js"
+  },
+  {
+    "revision": "ab25ec54bcfe0f3d4530",
+    "url": "/static/js/runtime-main.7a712340.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
